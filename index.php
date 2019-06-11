d422fb8f3facacb6fe6438f4aa12feadbea36352
Viral <viral.shah@adapty.com>
2019-06-11 13:16:10 +0530

    Elastic search

--- index.php
+++ index.php
@@ -172,6 +172,22 @@ include('includes/navbar.php');
               </div>
             </div>
           </div>
+
+          <!-- Elastic_Search -->
+            <div class="col-xl-3 col-md-6 mb-4">
+              <div class="card border-left-success shadow h-100 py-2">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-success text-uppercase mb-1"> <a href="http://52.172.128.181:5601/" target="_blank">ElasticSearch server </a></div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800">Logs Monitoring</div>
+                    </div>
+                    
+                  </div>
+                </div>
+              </div>
+            </div>
+
           <!-- Content Row -->
 
          
