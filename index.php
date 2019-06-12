a871828051ea148d13b866148b8a3d4739354ec6
Viral <viral.shah@adapty.com>
2019-06-12 12:36:51 +0530

    Color

--- index.php
+++ index.php
@@ -159,7 +159,7 @@ include('includes/navbar.php');
 
           <!-- Elastic_Search -->
             <div class="col-xl-3 col-md-6 mb-4">
-              <div class="card border-left-success shadow h-100 py-2">
+              <div class="card border-left-warning shadow h-100 py-2">
                 <div class="card-body">
                   <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
